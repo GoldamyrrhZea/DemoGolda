03b0d9a13c520af0ec5d9da5794b5fcefb63a10c
Meghan <darlinghirozerotwo002@gmail.com>
2024-04-23 19:55:28 +0800

    first

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+function MyButton() {
+  return (
+    <button style={styles.button}>
+      Click Here
+    </button>
+  );
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image
+        source={require('./assets/1.jpg')}
+        style={styles.image}
+      />
+      <MyButton />
+      <Text style={styles.text}>Get Started</Text>
     </View>
   );
 }
@@ -13,8 +26,25 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
     backgroundColor: '#fff',
+  },
+  button: {
+    backgroundColor: '#4CAF50',
+    paddingVertical: 15,
+    paddingHorizontal: 32,
+    borderRadius: 8,
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 20, // Adjust as needed
+  },
+  text: {
+    marginTop: 20, // Adjust as needed
+  },
+  image: {
+    width: 150,
+    height: 150,
+    resizeMode: 'contain',
   },
 });
